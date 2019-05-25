e35096c075a6d221ca205d3a2c6b9b60f234b4af
dettus <dettus@blech.deep.space>
2019-05-25 22:06:13 +0200

    still not applying the patches.

--- Debian/mkpackage.sh
+++ Debian/mkpackage.sh
@@ -4,12 +4,13 @@ wget -c http://www.dettus.net/dMagnetic/dMagnetic_0.13.tar.gz
 tar xvfz dMagnetic_0.13.tar.gz
 mv dMagnetic_0.13 dmagnetic-0.13
 tar cvfz dmagnetic_0.13.orig.tar.gz dmagnetic-0.13
-#cd dmagnetic-0.13
-#dh_make
+cd dmagnetic-0.13
+dh_make
+rm -rf debian
+cp -r ../debian .
 #debuild
 #quilt (to create the makefile patch)
-cp -r debian dmagnetic-0.13
-cd dmagnetic-0.13
+debian/rules clean
 debian/rules build
 fakeroot debian/rules binary
 
