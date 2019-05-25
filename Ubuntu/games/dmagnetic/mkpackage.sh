c5f6b59f0a9f488c6acd9849f8414de2097c180f
dettus <dettus@dettus.net>
2019-05-25 23:57:43 +0200

    initial ubuntu package.

---
+++ Ubuntu/games/dmagnetic/mkpackage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget -c http://www.dettus.net/dMagnetic/dMagnetic_0.13.tar.gz
+bzr whoami "Thomas Dettbarn <dettus@dettus.net>"
+bzr dh-make dmagnetic 0.13 dMagnetic_0.13.tar.gz
+rm -rf dmagnetic/debian
+cp -r debian dmagnetic
+
+
