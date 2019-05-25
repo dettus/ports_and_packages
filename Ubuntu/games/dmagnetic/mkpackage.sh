425aa391ba44d38d82d0a37ea51ef7b3bd95e2c9
dettus <dettus@dettus.net>
2019-05-26 00:15:53 +0200

    changes in the mkpackage.sh

--- Ubuntu/games/dmagnetic/mkpackage.sh
+++ Ubuntu/games/dmagnetic/mkpackage.sh
@@ -5,5 +5,5 @@ bzr whoami "Thomas Dettbarn <dettus@dettus.net>"
 bzr dh-make dmagnetic 0.13 dMagnetic_0.13.tar.gz
 rm -rf dmagnetic/debian
 cp -r debian dmagnetic
-
-
+cd dmagnetic
+bzr builddeb -- -us -uc
