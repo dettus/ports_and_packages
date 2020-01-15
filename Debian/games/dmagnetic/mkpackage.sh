1e8feed4314a7e37d556387eac0365d12c50f1f5
Thomas Dettbarn <dettus@dettus.netx>
2020-01-15 22:22:29 +0100

    debian package has been updated to release 0.20.

--- Debian/games/dmagnetic/mkpackage.sh
+++ Debian/games/dmagnetic/mkpackage.sh
@@ -1,72 +1,25 @@
 #!/bin/sh
 
-rm -rf dMagnetic_0.19.tar.bz2
-rm -rf dmagnetic_0.19.orig.tar.gz
-rm -rf dmagnetic-0.19
+rm -rf dMagnetic_0.20.tar.bz2
+rm -rf dmagnetic_0.20.orig.tar.gz
+rm -rf dmagnetic-0.20
 
 
 
-wget -c https://www.dettus.net/dMagnetic/dMagnetic_0.19.tar.bz2
-tar xvfj dMagnetic_0.19.tar.bz2
-mv dMagnetic_0.19 dmagnetic-0.19
-tar cvfz dmagnetic_0.19.orig.tar.gz dmagnetic-0.19
-tar cvfJ dmagnetic_0.19-1.debian.tar.xz debian
+wget -c https://www.dettus.net/dMagnetic/dMagnetic_0.20.tar.bz2
+tar xvfj dMagnetic_0.20.tar.bz2
+mv dMagnetic_0.20 dmagnetic-0.20
+tar cvfz dmagnetic_0.20.orig.tar.gz dmagnetic-0.20
+tar cvfJ dmagnetic_0.20-1.debian.tar.xz debian
 
-###(
-###echo -n "Format: " ; cat debian/source/format
-###echo "Source: dmagnetic"
-###echo "Binary: dmagnetic"
-###echo "Architecture: any"
-###echo "Version: 0.19-1"
-###grep "^Maintainer" debian/control
-###echo "Uploaders: Thomas Dettbarn <dettus@dettus.net>"
-###grep "^Homepage" debian/control
-###grep "^Standards-Version" debian/control
-###grep "^Build-Depends" debian/control
-###echo "Package-List:"
-###echo " dmagnetic deb games optional arch=any"
-###echo "Checksums-Sha1:"
-###for I in dmagnetic_0.19.orig.tar.gz dmagnetic_0.19-1.debian.tar.xz
-###do
-###	echo -n " "`sha1sum $I | awk -F" " '{ print $1; }' -`" "
-###	echo `wc -c $I`
-###done
-###
-###echo "Checksums-Sha256:"
-###for I in dmagnetic_0.17.orig.tar.gz dmagnetic_0.17-1.debian.tar.xz
-###do
-###	echo -n " "`sha256sum $I | awk -F" " '{ print $1; }' -`" "
-###	echo `wc -c $I`
-###done
-###
-###echo "Files:"
-###for I in dmagnetic_0.19.orig.tar.gz dmagnetic_0.19-1.debian.tar.xz
-###do
-###	echo -n " "`md5sum $I | awk -F" " '{ print $1; }' -`" "
-###	echo `wc -c $I`
-###done
-###) >/tmp/dmagnetic_0.19-1.dsc
-###
-cd dmagnetic-0.19
+cd dmagnetic-0.20
 pwd
 rm -rf debian
 cp -r ../debian .
 #debuild -uc -us
 debuild
 cd ..
-debsign dmagnetic_0.19-1_amd64.changes
-#dh_make
-
-#for I in `cat debian/patches/series`
-#do
-#	cat debian/patches/$I | patch
-#done
-#debuild
-#quilt (to create the makefile patch)
-#debian/rules clean
-#debian/rules build
-#fakeroot debian/rules binary
-
+debsign dmagnetic_0.20-1_amd64.changes
 
 
 
