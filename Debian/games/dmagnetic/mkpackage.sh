c6129b5c796982beaabadf5cbd731ca7f1521a2f
dettus <dettus@waffeleisen.deep.space>
2020-01-18 23:45:54 +0100

    release 0.21 preparation for debian.

--- Debian/games/dmagnetic/mkpackage.sh
+++ Debian/games/dmagnetic/mkpackage.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-rm -rf dMagnetic_0.20.tar.bz2
-rm -rf dmagnetic_0.20.orig.tar.gz
-rm -rf dmagnetic-0.20
+export VERSION=21
+
+rm -rf dMagnetic_0.${VERSION}.tar.bz2
+rm -rf dmagnetic_0.${VERSION}.orig.tar.gz
+rm -rf dmagnetic-0.${VERSION}
 
 
 
-wget -c https://www.dettus.net/dMagnetic/dMagnetic_0.20.tar.bz2
-tar xvfj dMagnetic_0.20.tar.bz2
-mv dMagnetic_0.20 dmagnetic-0.20
-tar cvfz dmagnetic_0.20.orig.tar.gz dmagnetic-0.20
-tar cvfJ dmagnetic_0.20-1.debian.tar.xz debian
+wget -c https://www.dettus.net/dMagnetic/dMagnetic_0.${VERSION}.tar.bz2
+tar xvfj dMagnetic_0.${VERSION}.tar.bz2
+mv dMagnetic_0.${VERSION} dmagnetic-0.${VERSION}
+tar cvfz dmagnetic_0.${VERSION}.orig.tar.gz dmagnetic-0.${VERSION}
+tar cvfJ dmagnetic_0.${VERSION}-1.debian.tar.xz debian
 
-cd dmagnetic-0.20
+cd dmagnetic-0.${VERSION}
 pwd
 rm -rf debian
 cp -r ../debian .
 #debuild -uc -us
 debuild
 cd ..
-debsign dmagnetic_0.20-1_amd64.changes
+debsign dmagnetic_0.${VERSION}-1_amd64.changes
 
 
 
