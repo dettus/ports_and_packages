204bac6047765c4d59da3c0ad1b8acfab2b25b92
dettus <dettus@waffeleisen.deep.space>
2020-06-08 07:29:43 +0200

    FreeBSD: added the new vmodes to the do-test.

--- FreeBSD/newrelease.sh
+++ FreeBSD/newrelease.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget http://www.dettus.net/dMagnetic/dMagnetic_0.22.tar.bz2
-sha256sum dMagnetic_0.22.tar.bz2
-ls -l dMagnetic_0.22.tar.bz2
+wget http://www.dettus.net/dMagnetic/dMagnetic_0.23.tar.bz2
+sha256sum dMagnetic_0.23.tar.bz2
+ls -l dMagnetic_0.23.tar.bz2
 
 
 mkdir svnserver
@@ -11,7 +11,7 @@ svn checkout svn://svn.FreeBSD.org/ports/head/games
 rm -rf games/dMagnetic/*
 cp -r ../games/dMagnetic/* games/dMagnetic
 cd ..
-svn diff svnserver/games/dMagnetic >dMagnetic_021_022.diff
+svn diff svnserver/games/dMagnetic >dMagnetic_022_023.diff
 
 echo
 echo "-----------------------------------------------------"
--- OpenBSD/newrelease.sh
+++ OpenBSD/newrelease.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget http://www.dettus.net/dMagnetic/dMagnetic_0.22.tar.bz2
-ls -l dMagnetic_0.22.tar.bz2
-sha256sum dMagnetic_0.22.tar.bz2
+wget http://www.dettus.net/dMagnetic/dMagnetic_0.23.tar.bz2
+ls -l dMagnetic_0.23.tar.bz2
+sha256sum dMagnetic_0.23.tar.bz2
 
 
 
@@ -12,8 +12,8 @@ cp ports/games/dmagnetic/distinfo distinfo.orig
 cp games/dmagnetic/Makefile Makefile
 cp games/dmagnetic/distinfo distinfo
 
-diff -u Makefile.orig Makefile >dmagnetic_020_to_022.patch
-diff -u distinfo.orig distinfo >>dmagnetic_020_to_022.patch
+diff -u Makefile.orig Makefile >dmagnetic_022_to_023.patch
+diff -u distinfo.orig distinfo >>dmagnetic_022_to_023.patch
 
 rm Makefile distinfo
 
