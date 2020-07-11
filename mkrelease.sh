7477b2752117340c78f1eb6514e8c7999203317a
dettus <dettus@waffeleisen.deep.space>
2020-07-11 19:53:52 +0200

    prepared for release 0.24.

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export ANCIENT_RELEASE=0.21
-export OLD_RELEASE=0.22
-export NEW_RELEASE=0.23
+export ANCIENT_RELEASE=0.22
+export OLD_RELEASE=0.23
+export NEW_RELEASE=0.24
 
 (
 	mkdir -p tmp/dettus.net/
