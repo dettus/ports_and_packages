81d5ed373f092bb76678bb6552a27c2f207890fc
dettus <dettus@waffeleisen.deep.space>
2020-07-26 22:54:49 +0200

    removed the share from the manpath of the freebsd port.

--- FreeBSD/newrelease.sh
+++ FreeBSD/newrelease.sh
@@ -10,9 +10,14 @@ rm -rf games/dMagnetic/*
 cp -r ../games/dMagnetic/* games/dMagnetic
 cd ..
 svn diff svnserver/games/dMagnetic >"dMagnetic_"$OLD_RELEASE"_"$NEW_RELEASE".diff"
-
+echo "in python:"
+echo "import datetime"
+echo "d=datetime.datetime(2020,7,26,14,12,0)"
+echo "d.timestamp()"
 echo
 echo "-----------------------------------------------------"
 echo "submit the port via https://bugs.freebsd.org/submit/"
 echo "please read https://www.freebsd.org/doc/en_US.ISO8859-1/books/porters-handbook/porting-submitting.html"
+
+
 
