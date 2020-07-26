ee70f9ab8f6a210ceec19920f09517b1639d3443
dettus <dettus@waffeleisen.deep.space>
2020-07-26 18:26:11 +0200

    prepared new releases for NetBSD.

---
+++ NetBSD/newrelease.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+(
+mkdir tmp
+cd tmp
+cvs -q -z2 -d anoncvs@anoncvs.NetBSD.org:/cvsroot checkout -P pkgsrc/games/dMagnetic
+)
+mkdir a
+mkdir b
+cp -r tmp/pkgsrc/games a
+cp -r games b
+
+diff -u a/games/dMagnetic b/games/dMagnetic >pkgsrc_games_dMagnetic_0.24_to_0.25.patch
+
+ls -l *.patch
+
+echo "now go to https://www.netbsd.org/cgi-bin/sendpr.cgi?gndb=netbsd" 
+echo "and send a short message to wiz@netbsd.org"
+
