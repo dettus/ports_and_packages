d4eaff0ebfdcdf45efe79e477d1d8d4261cf2e3d
dettus <dettus@waffeleisen.deep.space>
2020-11-28 12:54:42 +0100

    lets see.

--- nextgen/diff.sh
+++ nextgen/diff.sh
@@ -30,3 +30,10 @@ echo $VERSIONNUM
 
 ls -l repositories/*patch*.txt
 
+
+echo "send the OpenBSD patch to ports@openbsd.org, sthen@openbsd.org"
+echo "send the FreeBSD patch to https://bugs.freebsd.org/submit/ (Remember the +)"
+echo "send the NetBSD patch to https://www.netbsd.org/cgi-bin/sendpr.cgi?gndb=netbsd and a short message to wiz@netbsd.org"
+
+
+
