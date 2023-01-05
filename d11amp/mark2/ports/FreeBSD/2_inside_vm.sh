bab13435cf5278cc2736b4308f4bc38a3cae6c13
dettus <dettus@dampfgarer.deep.space>
2023-01-05 17:41:50 +0100

    prepared the ports generator.

---
+++ d11amp/mark2/ports/FreeBSD/2_inside_vm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd /usr/ports/audio/d11amp
+git pull
+tar xvfz /root/PACKAGE_IN.tar.gz
+git rm patches/*
+make makesum
+#(
+#  cd /usr/ports/pobj/d11amp-0.61/d11amp_0.61/
+#  sh /root/package_patch.sh
+#)
+
+#make update-patches
+#git add patches/*
+
+git diff --staged >/root/package_out.patch
+
+
