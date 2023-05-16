1486a788822a111bd59f19f94712de22bd6370a7
dettus <dettus@wasserkocher.deep.space>
2023-05-16 22:57:56 +0200

    prepared everything for dMagnetic 0.37

--- dmagnetic/mark2/3_runin.sh
+++ dmagnetic/mark2/3_runin.sh
@@ -12,9 +12,9 @@ ssh -i ../../VM_based/Keys/ssh-keyfile -p 2003 root@localhost "./4_inside_vm.sh"
 ssh -i ../../VM_based/Keys/ssh-keyfile -p 2004 user@localhost "./4_inside_vm.sh"
 
 
-scp -i ../../VM_based/Keys/ssh-keyfile -P 2001 root@localhost:package_out.patch openbsd_dmagnetic036.patch
-scp -i ../../VM_based/Keys/ssh-keyfile -P 2002 root@localhost:package_out.patch freebsd_dmagnetic036.patch
-scp -i ../../VM_based/Keys/ssh-keyfile -P 2003 root@localhost:package_out.patch netbsd_dmagnetic036.patch
+scp -i ../../VM_based/Keys/ssh-keyfile -P 2001 root@localhost:package_out.patch openbsd_dmagnetic037.patch
+scp -i ../../VM_based/Keys/ssh-keyfile -P 2002 root@localhost:package_out.patch freebsd_dmagnetic037.patch
+scp -i ../../VM_based/Keys/ssh-keyfile -P 2003 root@localhost:package_out.patch netbsd_dmagnetic037.patch
 
 
 
