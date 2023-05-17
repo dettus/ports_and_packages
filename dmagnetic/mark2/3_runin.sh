7df66ddbfac5350199d9678e5748d2807a4c8e77
dettus <dettus@wasserkocher.deep.space>
2023-05-17 23:58:17 +0200

    installing debian installs everything that is needed for the building of the package now.

--- dmagnetic/mark2/3_runin.sh
+++ dmagnetic/mark2/3_runin.sh
@@ -6,10 +6,10 @@ ssh -i ../../VM_based/Keys/ssh-keyfile -p 2002 root@localhost "chmod 755 4_insid
 ssh -i ../../VM_based/Keys/ssh-keyfile -p 2003 root@localhost "chmod 755 4_inside_vm.sh"
 ssh -i ../../VM_based/Keys/ssh-keyfile -p 2004 user@localhost "chmod 755 4_inside_vm.sh"
 
-ssh -i ../../VM_based/Keys/ssh-keyfile -p 2001 root@localhost "./4_inside_vm.sh"
-ssh -i ../../VM_based/Keys/ssh-keyfile -p 2002 root@localhost "./4_inside_vm.sh"
-ssh -i ../../VM_based/Keys/ssh-keyfile -p 2003 root@localhost "./4_inside_vm.sh"
-ssh -i ../../VM_based/Keys/ssh-keyfile -p 2004 user@localhost "./4_inside_vm.sh"
+ssh -i ../../VM_based/Keys/ssh-keyfile -p 2001 root@localhost "sh ./4_inside_vm.sh"
+ssh -i ../../VM_based/Keys/ssh-keyfile -p 2002 root@localhost "sh ./4_inside_vm.sh"
+ssh -i ../../VM_based/Keys/ssh-keyfile -p 2003 root@localhost "sh ./4_inside_vm.sh"
+ssh -i ../../VM_based/Keys/ssh-keyfile -p 2004 user@localhost "sh ./4_inside_vm.sh"
 
 
 scp -i ../../VM_based/Keys/ssh-keyfile -P 2001 root@localhost:package_out.patch openbsd_dmagnetic037.patch
