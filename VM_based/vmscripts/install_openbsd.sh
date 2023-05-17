778ad485ee5338c4ab6c19da481d120e0ff3662b
dettus <dettus@wasserkocher.deep.space>
2023-05-18 00:26:50 +0200

    the installation scripts for the openbsd VM contains the installation of pacakges as well.

--- VM_based/vmscripts/install_openbsd.sh
+++ VM_based/vmscripts/install_openbsd.sh
@@ -55,6 +55,16 @@ echo "***"
 ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "id"
 ssh -i Keys/ssh-keyfile -l user -p 2001 localhost "id"
 
+echo "***"
+echo "*** Installing packages"
+echo "***"
+
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "pkg_add git"
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "pkg_add git"
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "pkg_add bzip2"
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "pkg_add bzip2"
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "cd /usr"
+ssh -i Keys/ssh-keyfile -l root -p 2001 localhost "git clone https://github.com/openbsd/ports"
 
 echo "***"
 echo "*** Installation done. Shutting VM down"
