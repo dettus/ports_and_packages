9f215a65d544fbb3e22a319b0f018c99e844ebff
dettus <dettus@wasserkocher.deep.space>
2023-05-19 21:49:27 +0200

    execute the scripts

--- VM_based/2_install_build_env.sh
+++ VM_based/2_install_build_env.sh
@@ -2,8 +2,11 @@
 
 
 date
-vmscripts/2_build_env/install_build_env_debian.sh
-vmscripts/2_build_env/install_build_env_freebsd.sh
-vmscripts/2_build_env/install_build_env_netbsd.sh
-vmscripts/2_build_env/install_build_env_openbsd.sh
+bash vmscripts/2_build_env/install_build_env_debian.sh
+date
+bash vmscripts/2_build_env/install_build_env_freebsd.sh
+date
+bash vmscripts/2_build_env/install_build_env_netbsd.sh
+date
+bash vmscripts/2_build_env/install_build_env_openbsd.sh
 date
